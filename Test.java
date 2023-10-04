4f701a94003477a6f7057b7c98e4f33ce2e717a6
lakshmipathidevops <lakshmipathi.devops75@gmail.com>
2023-10-04 17:16:32 +0530

    modified

--- Test.java
+++ Test.java
@@ -1,8 +1,11 @@
 public class Test{
 
-    public static void main(String args[])
+   public static void main(String args[])
     {
+        int a= 10,b=20,c=30;
         System.out.println("welcome");
         System.out.println("devops");
+        int tot=a+b+c;
+        System.out.println("total:"+tot)
     }
 }
